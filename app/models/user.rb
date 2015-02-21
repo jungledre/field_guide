d3e816091233ea7f3a5b2940d063682248bc3fd1
Adrienne <adrie.k@gmail.com>
2015-02-20 16:52:05 -0800

    heroku push

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     uniqueness: {case_sensitive: false}
 
   validates_confirmation_of :password, on: :create
-  validates_presence_of :password_confirmation, on: :create
+  validates_presence_of     :password_confirmation, on: :create
 
   def self.authenticate email,password
     User.find_by_email(email).try(:authenticate, password)
